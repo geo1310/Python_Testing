c988f7b08f077c179072758f215aa5a056133d17
geo1310 <gbriche59@yahoo.fr>
2024-04-19 14:57:28 +0200

    Optimisation of test classes

--- gudlift_reservation/tests/test_calculation.py
+++ gudlift_reservation/tests/test_calculation.py
@@ -19,9 +19,19 @@ class TestCalculation(TestSetup):
     @pytest.mark.parametrize(
         "club_name, competition_name, places, expected_value",
         [
-            ("Club_test", "Competition_test", 10, "Great-booking complete!"),
-            ("Club_test_2", "Competition_test_2", 11, "insufficient number of points"),
-            (
+            (  # donnees valides
+                "Club_test",
+                "Competition_test",
+                10,
+                "Great-booking complete!",
+            ),
+            (  # nombre de places supérieur au points du club
+                "Club_test_2",
+                "Competition_test_2",
+                11,
+                "insufficient number of points",
+            ),
+            (  # nombre de places negatif
                 "Club_test",
                 "Competition_test",
                 -100,
@@ -48,50 +58,34 @@ class TestCalculation(TestSetup):
             follow_redirects=True,
         )
 
-        self.clubs = load_clubs()
-        club = next(club for club in self.clubs if club["name"] == club_name)
-        club_points_after = club["points"]
-        expected_points = club_points_before - places
-
         assert rv.status_code == 200
         assert expected_value.encode("utf-8") in rv.data
-        assert club_points_after >= 0
 
+        # verification du bon calcul des points du club
         if expected_value == "Great-booking complete!":
+            self.clubs = load_clubs()
+            club = next(club for club in self.clubs if club["name"] == club_name)
+            club_points_after = club["points"]
+            expected_points = club_points_before - places
+
             assert club_points_after == expected_points
+            assert club_points_after >= 0
 
     @pytest.mark.parametrize(
         "club_name, competition_name, places, expected_value",
         [
-            (
-                "Club_test_2",
-                "Competition_test",
+            (  # donnees valides
+                "Club_test",
+                "Competition_test_2",
+                5,
+                "Great-booking complete!",
+            ),
+            (  # nombre de places > 12
+                "Club_test",
+                "Competition_test_2",
                 13,
                 "use no more than 12 places per competition",
             ),
-        ],
-    )
-    def test_competition_no_more_12_places(
-        self, club_name, competition_name, places, expected_value
-    ):
-        """
-        Verifie l'utilisation des places d'une compétition
-        Le nombre de place utilisé par un club doit etre de 12 maximum pour une competition.
-        """
-
-        rv = self.client.post(
-            "/purchasePlaces",
-            data={"competition": competition_name, "club": club_name, "places": places},
-            follow_redirects=True,
-        )
-
-        assert rv.status_code == 200
-        assert expected_value.encode("utf-8") in rv.data
-
-    @pytest.mark.parametrize(
-        "club_name, competition_name, places, expected_value",
-        [
-            ("Club_test", "Competition_test", 1, "Great-booking complete!"),
         ],
     )
     def test_competition_reserved_places(
@@ -100,8 +94,78 @@ class TestCalculation(TestSetup):
         """
         Verifie la réservation des places dans une compétition
         Les places reservées doivent etre enregistrées dans le champ reserved_places de la compétition.
-        Le nombre de places réservées ne doit pas dépasser 12.
+        Le nombre de places réservées dans ne doit pas dépasser 12 par competition.
         """
+
+        rv = self.client.post(
+            "/purchasePlaces",
+            data={"competition": competition_name, "club": club_name, "places": places},
+            follow_redirects=True,
+        )
+
+        # verification des places reservées par le club dans reserved_places de la competition
+        self.competitions = load_competitions()
+        self.competition = next(
+            comp for comp in self.competitions if comp["name"] == competition_name
+        )
+        if "Great-booking complete!" in expected_value:
+            reserved_places = self.competition["reserved_places"][0]["reserved_places"]
+            assert reserved_places == places
+            assert reserved_places <= 12
+
+        assert rv.status_code == 200
+        assert expected_value.encode("utf-8") in rv.data
+
+    @pytest.mark.parametrize(
+        "club_name, competition_name, expected_value",
+        [
+            ("Club_test", "Competition_test", "Competition date has already passed"),
+        ],
+    )
+    def test_book_date_competition_not_be_in_past(
+        self, club_name, competition_name, expected_value
+    ):
+        """
+        Vérifie qu'on ne peut pas réserver de places dans une compétition deja passée.
+        Dans la méthode GET de de la route de réservation book
+        """
+        self.competition = next(
+            comp for comp in self.competitions if comp["name"] == competition_name
+        )
+
+        # modification de la date de la competition
+        date_test = datetime.now() - timedelta(days=50)
+        self.competition["date"] = date_test.strftime("%Y-%m-%d %H:%M:%S")
+        save_competitions(self.competitions)
+
+        response = self.client.get(f"/book/{competition_name}/{club_name}")
+
+        assert response.status_code == 200
+        assert expected_value.encode("utf-8") in response.data
+
+    @pytest.mark.parametrize(
+        "club_name, competition_name, places, expected_value",
+        [
+            ("Club_test", "Competition_test", 1, "Competition date has already passed"),
+        ],
+    )
+    def test_purchase_places_date_competition_not_be_in_past(
+        self, club_name, competition_name, places, expected_value
+    ):
+        """
+        Vérifie qu'on ne peut pas réserver de places dans une compétition deja passée.
+        Dans la méthode POST de de la route purchase_places
+        """
+        self.competition = next(
+            comp for comp in self.competitions if comp["name"] == competition_name
+        )
+
+        # modification de la date de la competition
+        date_test = datetime.now() - timedelta(days=50)
+        self.competition["date"] = date_test.strftime("%Y-%m-%d %H:%M:%S")
+        save_competitions(self.competitions)
+
+        self.competitions = load_competitions()
 
         rv = self.client.post(
             "/purchasePlaces",
@@ -112,31 +176,10 @@ class TestCalculation(TestSetup):
         assert rv.status_code == 200
         assert expected_value.encode("utf-8") in rv.data
 
-        # TODO ajouter verif dans reserved_places
-
-    def test_date_competition_not_be_in_past(self):
-        """
-        Vérifie qu'on ne peut pas réserver de places dans une compétition deja passée.
-        """
-        self.competition = next(
-            comp for comp in self.competitions if comp["name"] == "Competition_test"
-        )
-
-        date_test = datetime.now() - timedelta(days=50)
-        self.competition["date"] = date_test.strftime("%Y-%m-%d %H:%M:%S")
-        save_competitions(self.competitions)
-
-        self.competitions = load_competitions()
-
-        response = self.client.get("/book/Competition_test/Club_test")
-
-        assert response.status_code == 200
-        assert b"Competition date has already passed" in response.data
-
     @pytest.mark.parametrize(
         "club_name, competition_name, places, expected_value",
         [
-            (
+            (  # nombre de places superieur au places disponibles de la competition
                 "Club_test",
                 "Competition_test",
                 12,
@@ -148,7 +191,7 @@ class TestCalculation(TestSetup):
         self, club_name, competition_name, places, expected_value
     ):
         """
-        Verifie si le nombre de places dans une competition est suffisant
+        Verifie si le nombre de places dans une competition est suffisant.
         Il ne peut pas etre négatif.
         """
 
--- gudlift_reservation/tests/test_server_routes.py
+++ gudlift_reservation/tests/test_server_routes.py
@@ -8,147 +8,117 @@ class TestServerRoutes(TestSetup):
     Classe de tests pour tester les routes de l'application.
     """
 
-    def test_ok_index_route(self):
+    def test_index_route(self):
         """
         Test de la route "/".
         Vérifie le code de statut de la réponse 200
-        et si le contenu de la réponse contient un message spécifique.
+        et si le contenu de la réponse contient le message de connexion.
         """
         response = self.client.get("/")
         assert response.status_code == 200
         assert b"Please enter your secretary email to continue" in response.data
 
-    def test_ok_show_summary_route(self):
-        """
-        Test de la route "/showSummary".
-        Envoie une requête POST avec une adresse e-mail valide
-        et vérifie le code de statut de la réponse 200.
-        """
-        rv = self.client.post(
-            "/showSummary", data=dict(email="club_test@email.fr"), follow_redirects=True
-        )
-        assert rv.status_code == 200
-
     @pytest.mark.parametrize(
         "email, expected_value",
         [
-            ("", "No email provided"),
-            ("fail_test@email.fr", "Club with this email fail_test@email.fr not found"),
+            ("club_test@email.fr", "Welcome, club_test@email.fr "),  # données valides
+            ("", "No email provided"),  # absence d'email
+            ("fail_test@email.fr", "Club with this email fail_test@email.fr not found"),  # email non valide
         ],
     )
-    def test_fail_show_summary_route(self, email, expected_value):
+    def test_show_summary_route(self, email, expected_value):
         """
         Test de la route "/showSummary".
-        Envoie une requête POST avec une adresse e-mail absente ou incorrecte
-        Vérifie la redirection vers index.html avec le code de statut 200 et un message d'erreur.
+        Envoie une requête POST avec une adresse e-mail valide, non valide et sans email.
+        et vérifie le code de statut de la réponse 200 et un message spécifique.
         """
         rv = self.client.post(
             "/showSummary", data=dict(email=email), follow_redirects=True
         )
+
         assert rv.status_code == 200
         assert expected_value.encode("utf-8") in rv.data
-
-    def test_ok_book_route(self):
-        """
-        Test de la route "/book/<competition>/<club>" avec une competition et un club valides.
-        Vérifie le code de statut de la réponse 200
-        et si le contenu de la réponse contient un message spécifique.
-        """
-        club = "Club_test"
-        competition = "Competition_test"
-        response = self.client.get(f"/book/{competition}/{club}")
-        assert response.status_code == 200
-        assert b"How many places" in response.data
 
     @pytest.mark.parametrize(
         "club, competition, expected_value, status_code",
         [
-            ("xxx", "Competition_test", "Invalid club", 400),
-            (
+            ("Club_test", "Competition_test", "How many places", 200),  # données valides
+            ("xxx", "Competition_test", "Invalid club", 400),  # club non valide
+            (  # absence de club
                 "",
                 "Competition_test",
-                "",
+                "Not Found",
                 404,
             ),
-            (
+            (  # competition non valide
                 "Club_test",
                 "xxx",
                 "Invalid competition",
                 400,
             ),
-            (
+            (  # absence de competition
                 "Club_test",
                 "",
-                "",
+                "Not Found",
                 404,
             ),
         ],
     )
-    def test_fail_book_route(self, club, competition, expected_value, status_code):
+    def book_route(self, club, competition, expected_value, status_code):
         """
-        Test de la route "/book/<competition>/<club>" avec des données non valides.
+        Test de la route "/book/<competition>/<club>" avec des données valides et non valides.
         Vérifie le code de statut de la réponse 400 ou 404 et un message d'erreur.
         """
         response = self.client.get(f"/book/{competition}/{club}")
         assert response.status_code == status_code
         expected_value.encode("utf-8") in response.data
 
-    def test_ok_purchase_places_route(self):
-        """
-        Test de la route "/purchasePlaces".
-        Envoie une requête POST avec avec une competition et un club valides
-        Vérifie le code de statut de la réponse 200
-        et si le contenu de la réponse contient un message spécifique.
-        """
-        club = "Club_test"
-        competition = "Competition_test"
-        places = 1
-        rv = self.client.post(
-            "/purchasePlaces",
-            data={"competition": competition, "club": club, "places": places},
-        )
-        assert rv.status_code == 200
-        assert b"Great-booking complete!" in rv.data
-
     @pytest.mark.parametrize(
         "club, competition, places, expected_value, status_code",
         [
-            (
+            (  # données valides
+                "Club_test",
+                "Competition_test",
+                1,
+                "Great-booking complete!",
+                200,
+            ),
+            (  # club non valide
                 "xxx",
                 "Competition_test",
                 1,
                 "Invalid club",
                 400,
             ),
-            (
+            (  # competition non valide
                 "Club_test",
                 "xxx",
                 1,
                 "Invalid competition",
                 400,
             ),
-            (
+            (  # absence de competition
                 "Club_test",
                 "",
                 1,
-                "",
+                "Invalid competition",
                 400,
             ),
-            (
+            (  # absence de club
                 "",
                 "Competition_test",
                 1,
-                "",
+                "Invalid club",
                 400,
             ),
-            (
+            (  # absence de places
                 "Club_test",
                 "Competition_test",
                 "",
                 "Invalid number",
                 200,
             ),
-            (
+            (  # places de mauvais type
                 "Club_test",
                 "Competition_test",
                 "xxx",
@@ -157,13 +127,13 @@ class TestServerRoutes(TestSetup):
             ),
         ],
     )
-    def test_fail_purchase_places_route(
+    def test_purchase_places_route(
         self, club, competition, places, expected_value, status_code
     ):
         """
         Test de la route "/purchasePlaces".
-        Envoie une requête POST avec avec des données non valides
-        Vérifie le code de statut de la réponse 400 ou 200 et un message d'erreur
+        Envoie une requête POST avec avec des données valides et non valides
+        Vérifie le code de statut de la réponse 400 ou 200 et un message spécifique.
         """
 
         rv = self.client.post(
@@ -174,7 +144,7 @@ class TestServerRoutes(TestSetup):
         assert rv.status_code == status_code
         assert expected_value.encode("utf-8") in rv.data
 
-    def test_ok_logout_route(self):
+    def test_logout_route(self):
         """
         Test de la route "/logout".
         Vérifie si la route renvoie un code de statut 200
